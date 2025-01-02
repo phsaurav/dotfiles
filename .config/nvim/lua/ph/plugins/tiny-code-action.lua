fdf3c9fdbe69597795a0adfd63aad6a92a4ec7f3
PH Saurav <phsaurav@users.noreply.github.com>
2025-01-02 16:00:21 +0600

    feat: Minor updates and balanced CLI tools

--- .config/nvim/lua/ph/plugins/tiny-code-action.lua
+++ .config/nvim/lua/ph/plugins/tiny-code-action.lua
@@ -1,28 +1,28 @@
 return {
-	"rachartier/tiny-code-action.nvim",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope.nvim" },
-	},
-	event = "LspAttach",
-	config = function()
-		require("tiny-code-action").setup({
-			signs = {
-				quickfix = { "󰁨", { link = "DiagnosticInfo" } },
-				others = { "?", { link = "DiagnosticWarning" } },
-				refactor = { "", { link = "DiagnosticWarning" } },
-				["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
-				["refactor.extract"] = { "", { link = "DiagnosticError" } },
-				["source.organizeImports"] = { "", { link = "TelescopeResultVariable" } },
-				["source.fixAll"] = { "", { link = "TelescopeResultVariable" } },
-				["source"] = { "", { link = "DiagnosticError" } },
-				["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
-				["codeAction"] = { "", { link = "DiagnosticError" } },
-			},
-		})
+  "rachartier/tiny-code-action.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+  },
+  event = "LspAttach",
+  config = function()
+    require("tiny-code-action").setup({
+      signs = {
+        quickfix = { "󰁨", { link = "DiagnosticInfo" } },
+        others = { "?", { link = "DiagnosticWarning" } },
+        refactor = { "", { link = "DiagnosticWarning" } },
+        ["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
+        ["refactor.extract"] = { "", { link = "DiagnosticError" } },
+        ["source.organizeImports"] = { "", { link = "TelescopeResultVariable" } },
+        ["source.fixAll"] = { "", { link = "TelescopeResultVariable" } },
+        ["source"] = { "", { link = "DiagnosticError" } },
+        ["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
+        ["codeAction"] = { "", { link = "DiagnosticError" } },
+      },
+    })
 
-		vim.keymap.set("n", "<leader>ca", function()
-			require("tiny-code-action").code_action()
-		end, { noremap = true, silent = true })
-	end,
+    vim.keymap.set("n", "<leader>ca", function()
+      require("tiny-code-action").code_action()
+    end, { noremap = true, silent = true })
+  end,
 }
