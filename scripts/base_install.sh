b9ece0235079a5633c74b86d5e58ca5bda2072fd
PH Saurav <phsaurav@users.noreply.github.com>
2025-12-26 00:11:47 +0600

    feat: Zap Installation added

--- scripts/base_install.sh
+++ scripts/base_install.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+
 # Install stow
 yay -S --noconfirm --needed stow
+
 # Install Zsh
 if ! command -v zsh &>/dev/null; then
     yay -S --noconfirm --needed zsh
 fi
+
+# Install Zap (minimal Zsh plugin manager)
+if [ ! -f "${HOME}/.local/share/zap/zap.zsh" ]; then
+    echo "Installing Zap..."
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --keep
+else
+    echo "Zap is already installed."
+fi
+
 # Install Zen Browser
 if ! command -v zen &>/dev/null; then
     yay -S --noconfirm --needed zen-browser-bin
 fi
+
 # Install VSCode
 if ! command -v code &>/dev/null; then
     yay -S --noconfirm --needed visual-studio-code-bin
